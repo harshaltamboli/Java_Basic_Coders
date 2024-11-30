d0804100782e089cf8776dca4338987b6a23eff9
harshaltamboli <harshaltamboli429@gmail.com>
2024-11-30 13:50:06 +0530

    [ADDED]:added a simple variable in java

---
+++ varable.java
@@ -0,0 +1,14 @@
+package BassicsOfjava;
+
+public class varable {
+    public static void main(String[] args) {
+        int x;
+        int z;
+        x=500;
+        System.out.println(x);
+        x=400;
+        System.out.println(x);
+        z=x+7;
+        System.out.println(z);
+    }
+}
