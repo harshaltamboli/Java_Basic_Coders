47e4f0f5bf18f77c95a72a5453500a15d3a2a26b
harshaltamboli <harshaltamboli429@gmail.com>
2024-11-30 13:52:06 +0530

    this is a simple arithmatic operation in java

---
+++ Arithmati.java
@@ -0,0 +1,34 @@
+package BassicsOfjava;
+
+public class Arithmati {
+    public static void main (String[] args){
+
+        int x=50 ;
+        int y=10;
+
+        // incement / decrement op
+        x++;    
+        System.out.println(x);
+        x--;
+        System.out.println(x);
+
+        ++x;
+        System.out.println(x);
+
+
+
+        System.out.println(x+y);
+        System.out.println(x-y);
+        System.out.println(x*y);
+        System.out.println(x/y);
+
+        // both are diffrent becouse store a contenar in string value can not perform a arithmatic value
+        // print a 5010 in secound statement
+
+        System.out.println(x+y);
+        System.out.println(""+x+y);
+
+
+    }
+
+}
