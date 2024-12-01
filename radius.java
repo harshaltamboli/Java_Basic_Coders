c08f4621a33b22afa2243616ab5c5dc1a5c8acef
harshaltamboli <harshaltamboli429@gmail.com>
2024-12-01 16:14:10 +0530

    this is a simple calculat a radius find

---
+++ radius.java
@@ -0,0 +1,12 @@
+package BassicsOfjava;
+
+public class radius {
+    public static void main(String[] args) {
+        double r=8;
+        double pi=3.1415;
+        double area=pi*r*r;
+        System.out.println(area);
+    }
+
+
+}
