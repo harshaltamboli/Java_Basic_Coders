43c8cb12168c8e2ef101dd429f3fd8524d98e36b
harshaltamboli <harshaltamboli429@gmail.com>
2024-12-01 16:10:34 +0530

    this is a simple calculate 5 subject in percentage

---
+++ percentage_of_5_subject.java
@@ -0,0 +1,14 @@
+package BassicsOfjava;
+
+public class percentage_of_5_subject {
+    public static void main(String[] args) {
+        double  profesional_communicattion = 88;
+        double  e_m_commerce = 88;
+        double cloud_computin=89;
+        double angularjs=79;
+        double p=(profesional_communicattion+e_m_commerce+cloud_computin+angularjs)/4;
+
+        System.out.println(p);
+    }
+
+}
